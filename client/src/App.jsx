65ae083674524a605a50d9c5b98a7f18774e5d17
Divakar <vdivakar317@gmail.com>
2026-04-13 15:02:45 +0530

    add routing and page components for Home, Map, Report, Profile, Admin; implement BottomNav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import Report from "./pages/Report";
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
+
+import BottomNav from "./components/layout/BottomNav";
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-green-600 font-bold">
-      Tailwind Working 🚀
-    </h1>
+    <Router>
+      <div className="pb-20 bg-gray-100 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+
+        <BottomNav />
+      </div>
+    </Router>
   );
 }
